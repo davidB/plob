c4b9ab0cae28742b965cc1162f1ac4bf7c15108f
David Bernard <david.bernard.31@gmail.com>
2011-10-05 21:20:42 +0200

    the startup code

---
+++ src/test/scala/samples/junit.scala
@@ -0,0 +1,28 @@
+package samples
+
+import org.junit._
+import Assert._
+import java.nio.file.FileSystems
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+    @Test
+    def testPath0() = {
+      val s0 = "/tmp/tests/foo/bar.txt"
+      val p0 = FileSystems.getDefault.getPath(s0)
+      assertEquals(p0.toString, s0)
+      assertEquals(p0.getFileName().toString, "bar.txt")
+      
+      val p1 = p0.getParent().resolve("bar2.txt")
+      assertEquals(p1.relativize(p0).toString, "../bar.txt")
+      assertEquals(p0.relativize(p1).toString, "../bar2.txt")
+      assertEquals(p0.getParent.relativize(p1).toString, "bar2.txt")
+    }
+
+}
+
+
